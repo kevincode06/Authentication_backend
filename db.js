fe7923a242e9ad03caf56b2e6f506aa177acb990
“Kevincode6” <maloukevin6@gmail.com>
2025-04-09 17:22:12 +0200

    update of dotenv

--- db.js
+++ db.js
@@ -1,3 +1,22 @@
+require("dotenv").config(); // Add this at the top of your file
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  mongoose 
+    .connect(process.env.DB, connectionParams)
+    .then(() => {
+      console.log("Database connected successfully");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
+};
+require("dotenv").config(); // Add this at the top of your file
 const mongoose = require("mongoose");
 
 module.exports = () => {
@@ -15,4 +34,3 @@ module.exports = () => {
       console.error("MongoDB connection error:", error);
     });
 };
-
